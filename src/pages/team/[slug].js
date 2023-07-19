cb05ebc2e8c9bc44757f8d56bde7506433cca3f8
Braeden Craig <braeden.craig@gmail.com>
2023-07-18 20:32:22 -0400

    update

--- src/pages/team/[slug].js
+++ src/pages/team/[slug].js
@@ -84,28 +84,29 @@ export default function TeamMember({ teamMember, onePassword }) {
           </div>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 lg:gap-8 xl:gap-16 max-w-[1600px] w-full">
-          {teamMember[0].fields.bands.map(
-            ({ fields: { title, slug, folders, image } }, i) => {
-              return (
-                <Link key={i} href={`/bands/${slug}`}>
-                  <div className="relative lg:max-h-[300px] lg:h-[300px] w-[100%] max-w-[100%] h-[400px] lg:mb-0 mb-16 border-white-500 border-2 rounded-xl">
-                    <h2 className="absolute left-0 right-0 text-center top-8 lg:top-4 text-4xl lg:text-2xl uppercase z-40 font-black font-mono">
-                      {title}
-                    </h2>
-                    {image?.fields.file.url && (
-                      <Image
-                        className="opacity-75 hover:opacity-50 transition duration-300 relative rounded-xl"
-                        src={"https:" + image?.fields.file.url}
-                        alt="band"
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                    )}
-                  </div>
-                </Link>
-              );
-            }
-          )}
+          {teamMember[0].fields.bands &&
+            teamMember[0].fields.bands.map(
+              ({ fields: { title, slug, folders, image } }, i) => {
+                return (
+                  <Link key={i} href={`/bands/${slug}`}>
+                    <div className="relative lg:max-h-[300px] lg:h-[300px] w-[100%] max-w-[100%] h-[400px] lg:mb-0 mb-16 border-white-500 border-2 rounded-xl">
+                      <h2 className="absolute left-0 right-0 text-center top-8 lg:top-4 text-4xl lg:text-2xl uppercase z-40 font-black font-mono">
+                        {title}
+                      </h2>
+                      {image?.fields.file.url && (
+                        <Image
+                          className="opacity-75 hover:opacity-50 transition duration-300 relative rounded-xl"
+                          src={"https:" + image?.fields.file.url}
+                          alt="band"
+                          layout="fill"
+                          objectFit="cover"
+                        />
+                      )}
+                    </div>
+                  </Link>
+                );
+              }
+            )}
         </div>
       </main>
     );
