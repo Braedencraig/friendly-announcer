d0857b59a6932a2e0d0c8321657f5a9a030ad796
Braeden Craig <braeden.craig@gmail.com>
2023-07-20 19:35:28 -0400

    update bands order contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -168,26 +168,28 @@ export default function Home({ teamMembers, bands, onePassword }) {
               </div>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 lg:gap-8 xl:gap-16 max-w-[1600px] w-full">
-              {bands.map(({ fields: { title, slug, folders, image } }, i) => {
-                return (
-                  <Link key={i} href={`/bands/${slug}`}>
-                    <div className="relative lg:max-h-[300px] lg:h-[300px] w-[100%] max-w-[100%] h-[400px] lg:mb-0 mb-16 border-white-500 border-2 rounded-xl">
-                      <h2 className="absolute left-0 right-0 text-center top-8 lg:top-4 text-4xl lg:text-2xl uppercase z-40 font-black font-mono">
-                        {title}
-                      </h2>
-                      {image?.fields.file.url && (
-                        <Image
-                          className="opacity-75 hover:opacity-50 transition duration-300 relative rounded-xl"
-                          src={"https:" + image?.fields.file.url}
-                          alt="band"
-                          layout="fill"
-                          objectFit="cover"
-                        />
-                      )}
-                    </div>
-                  </Link>
-                );
-              })}
+              {bands[0].fields.band.map(
+                ({ fields: { title, slug, folders, image } }, i) => {
+                  return (
+                    <Link key={i} href={`/bands/${slug}`}>
+                      <div className="relative lg:max-h-[300px] lg:h-[300px] w-[100%] max-w-[100%] h-[400px] lg:mb-0 mb-16 border-white-500 border-2 rounded-xl">
+                        <h2 className="absolute left-0 right-0 text-center top-8 lg:top-4 text-4xl lg:text-2xl uppercase z-40 font-black font-mono">
+                          {title}
+                        </h2>
+                        {image?.fields.file.url && (
+                          <Image
+                            className="opacity-75 hover:opacity-50 transition duration-300 relative rounded-xl"
+                            src={"https:" + image?.fields.file.url}
+                            alt="band"
+                            layout="fill"
+                            objectFit="cover"
+                          />
+                        )}
+                      </div>
+                    </Link>
+                  );
+                }
+              )}
             </div>
           </>
         )}
@@ -210,7 +212,7 @@ export async function getStaticProps() {
         (item) => item.sys.contentType.sys.id === "teamMembers"
       ),
       bands: data.items.filter(
-        (item) => item.sys.contentType.sys.id === "band"
+        (item) => item.sys.contentType.sys.id === "bands"
       ),
       onePassword: data.items.filter(
         (item) => item.sys.contentType.sys.id === "onePassword"
