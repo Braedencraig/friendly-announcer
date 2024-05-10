c07c89a71fc0b42a733f296924e8e1df3ea27415
Braeden Craig <braeden.craig@gmail.com>
2024-05-10 18:08:51 -0400

    fix issue w search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -21,31 +21,40 @@ export default function SearchBar({ setQuery, query, data }) {
       {query.length === 0 ? null : (
         <div className="bg-white text-black font-mono absolute z-[1000] mt-10">
           {data.map((item, i) => {
-            return item.fields.folders.map((folder, i) => {
-              if (folder.fields.folderContents === undefined) return null;
-              return folder.fields.folderContents.map((content, i) => {
-                if (
-                  content.fields.title
-                    .toLowerCase()
-                    .includes(query.toLowerCase())
-                ) {
-                  return (
-                    <div
-                      className="py-2 px-4 bg-white hover:bg-black hover:text-white"
-                      key={i}
-                    >
-                      <a
-                        href={content.fields.url}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <h1>{content.fields.title}</h1>
-                      </a>
-                    </div>
-                  );
-                }
-              });
-            });
+            return item.fields.folders && Array.isArray(item.fields.folders)
+              ? item.fields.folders.map((folder, i) => {
+                  if (!folder.fields || !folder.fields.folderContents)
+                    return null;
+
+                  return folder.fields.folderContents
+                    .map((content, i) => {
+                      if (
+                        content.fields &&
+                        content.fields.title &&
+                        content.fields.title
+                          .toLowerCase()
+                          .includes(query.toLowerCase())
+                      ) {
+                        return (
+                          <div
+                            className="py-2 px-4 bg-white hover:bg-black hover:text-white"
+                            key={i}
+                          >
+                            <a
+                              href={content.fields.url}
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <h1>{content.fields.title}</h1>
+                            </a>
+                          </div>
+                        );
+                      }
+                      return null;
+                    })
+                    .filter(Boolean);
+                })
+              : null;
           })}
         </div>
       )}
